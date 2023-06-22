93537f43ef4d9373de066fb1e2162757f56d5566
RainerAtSpirit <R.Wittmann@spirit.de>
2023-06-22 06:13:20 +0200

    make it work with JavaScript disabled

--- src/app/Button.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export function Button({ text, send }: { text: string; send: any }) {
-  return <button onClick={() => send(text)}>{text}</button>
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import { createMachine, interpret } from 'xstate'
-import { Button } from './Button'
-import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+import { cache } from 'react'
+import { kv } from '@vercel/kv'
+
+const STATE_ID = 'STATE_ID'
+
+export const getState = cache(async (id: string) => {
+  const state = await kv.json.get(id)
+  const persistedState = JSON.parse(state.persistedState)
+
+  return persistedState || undefined
+})
 
 const donutMachine = createMachine({
   id: 'donut',
@@ -81,7 +91,7 @@ const donutMachine = createMachine({
 
 let restoredState: any
 try {
-  restoredState = undefined
+  restoredState = await getState(STATE_ID)
 } catch (e) {
   console.log('No persisted state found.')
   restoredState = undefined
@@ -92,32 +102,65 @@ const actor = interpret(donutMachine, {
 })
 let nextEvents: any[] = []
 actor.subscribe({
-  next(snapshot) {
-    console.log('next', nextEvents)
+  async next(snapshot) {
+    'use server'
+
     nextEvents = snapshot.nextEvents.filter(
       (event) => !event.startsWith('done.')
     )
 
-    const persistedState = actor.getPersistedState()
+    const persistedState = JSON.stringify(actor.getPersistedState())
+
+    await kv.json.set(STATE_ID, '$', {
+      persistedState,
+    })
+
+    // console.log('persistedState', persistedState)
   },
 })
 actor.start()
 
-export async function sendAction(eventName: string) {
+export async function sendAction(formData: FormData) {
   'use server'
-  actor.send({ type: eventName })
-  console.log(eventName)
-  redirect('')
+  const type = formData.get('type') as string
+  const nextEvent = { type }
+
+  const waiting = actor.subscribe({
+    async next(snapshot) {
+      'use server'
+      console.log('next', nextEvents)
+      revalidatePath('/')
+      // Todo: redirect from next/navigation doesn't work
+      // redirect('/')
+      //
+    },
+  })
+
+  actor.send(nextEvent)
+
+  waiting.unsubscribe()
 }
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>{JSON.stringify(actor.getSnapshot().value)} </h1>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Next.js alpha meets <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">xstate5 beta</span></h1>
+      <h2 className="text-4xl font-extrabold dark:text-white">What possible can go wrong</h2>
+      <h3 className="text-3xl font-bold dark:text-white">{JSON.stringify(actor.getSnapshot().value)} </h3>
       <div>
-        {nextEvents.map((next) => {
-          return <Button key={next} text={next} send={sendAction}></Button>
-        })}
+        <form action={sendAction}>
+          {nextEvents.map((next) => {
+            return (
+              <input
+                type="submit"
+                name="type"
+                key={next}
+                value={next}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              />
+            )
+          })}
+        </form>
       </div>
     </main>
   )
